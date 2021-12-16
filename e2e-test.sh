a4545814de56e53d2c3ba348d20923efcf2b8f0a
Pep Turró Mauri <pep@redhat.com>
2021-12-17 00:05:09 +0100

    Adapt e2e script to vanilla kube
    Also install prometheus for ServiceMonitor

--- e2e-test.sh
+++ e2e-test.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
+set -ex
+
+# PREREQUISITE: `helm` and `kubectl` are installed
+
+# PREREQUISITE: a kubernetes / openshift cluster is available and configured
+
 # TEST-ENV: bring up a cluster
-GO111MODULE="on" go install sigs.k8s.io/kind@v0.11.1
-kind create cluster
+# GO111MODULE="on" go install sigs.k8s.io/kind@v0.11.1
+# kind create cluster
 
-# PREREQUISITES: deploy tekton
-kubectl create namespace tekton-pipelines
-oc apply --filename https://storage.googleapis.com/tekton-releases/pipeline/previous/v0.29.0/release.notags.yaml
-oc apply --filename https://storage.googleapis.com/tekton-releases/triggers/previous/v0.17.0/release.yaml
-oc apply --filename https://storage.googleapis.com/tekton-releases/triggers/previous/v0.17.0/interceptors.yaml
-oc adm policy add-scc-to-user anyuid -z tekton-pipelines-controller
+# PREREQUISITE: deploy tekton
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/previous/v0.28.2/release.notags.yaml
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/previous/v0.16.1/release.yaml
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/previous/v0.16.1/interceptors.yaml
+#oc adm policy add-scc-to-user anyuid -z tekton-pipelines-controller
 
-# TEST: deploy aicoe-ci
-kubectl create namespace thoth-aidevsecops-pipelines
-kubectl config set-context --current --namespace=thoth-aidevsecops-pipelines
-oc adm policy add-scc-to-user privileged -z aicoe-ci -n tekton-pipelines
-helm install thoth-pipelines charts/meteor-pipelines
+# PREREQUISITE: deploy prometheus (needed for ServiceMonitor)
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install --create-namespace --namespace metrics \
+     prometheus-stack prometheus-community/kube-prometheus-stack
+
+# TEST: deploy meteor-pipelines
+#oc adm policy add-scc-to-user privileged -z aicoe-ci -n tekton-pipelines
+helm install --create-namespace --namespace=thoth-aidevsecops-pipelines \
+     thoth-pipelines charts/meteor-pipelines
 
 # TEST-CASE: at least this secret should be there
-kubectl get secret quay-pusher-secret --output="jsonpath={.data.\.dockerconfigjson}" | base64 --decode | jq .auths
+# kubectl get secret quay-pusher-secret --output="jsonpath={.data.\.dockerconfigjson}" | base64 --decode | jq .auths
 
 # TEST-ENV: tear down the cluster
 # kind delete cluster
